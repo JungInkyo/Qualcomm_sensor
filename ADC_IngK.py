4d1ceb473151170acf79676248586e4865196486
JungInkyo <vj5645@naver.com>
2017-07-22 23:28:54 -0700

    ADC_IngK modify
    17. 07. 22
    show temprature

--- ADC_IngK.py
+++ ADC_IngK.py
@@ -2,5 +2,7 @@ from time import sleep #import sleep
 
 while True: #it runs all the time
     raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
+    v = (5000*raw) / 4096
+    t = v -642
+    print t
     sleep(1)
-    print raw
