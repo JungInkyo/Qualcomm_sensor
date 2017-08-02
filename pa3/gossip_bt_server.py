6297e0eac42be4caa7c6a0f31e3ec28cea62401a
JungInkyo <vj5645@naver.com>
2017-08-02 00:12:20 -0700

    modify gossip.py
    gogogo

--- pa3/gossip_bt_server.py
+++ pa3/gossip_bt_server.py
@@ -53,12 +53,12 @@ if __name__ == '__main__':
                 #msg = "%d, %f, %f, %f, %f, %f, %f" % (epoch_time, temp, SN1, SN2, SN3, SN4, PM25)
                 msg = "%d, %s, %f, %f, %f, %f, %f, %f" % (0,epoch_time, temp, SN1, SN2, SN3, SN4, PM25)
             elif args.output_format == "json":
-                output = {'type': 'realtime',
+                output = {'type': 0,
                           'time': epoch_time,
-                          'SN1': SN1,
-                          'SN2': SN2,
-                          'SN3': SN3,
-                          'SN4': SN4,
+                          'CO2': SN1,
+                          'SO2': SN2,
+                          'NO2': SN3,
+                          'O3': SN4,
                           'PM25': PM25}
                 msg = json.dumps(output)
             try:
