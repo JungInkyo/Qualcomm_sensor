3b64dad8c2dfd3a69e64244587c5b721a2ea819a
JungInkyo <vj5645@naver.com>
2017-08-08 01:46:03 -0700

    ggg
    ffff

--- pa10b/sensor/Sensor.py
+++ pa10b/sensor/Sensor.py
@@ -163,21 +163,21 @@ class SensorServer(Thread):
 
             logger.info("Reading {} sensor...".format(self.sensor_names[2]))
             c4, c5 = self.read_sensor(2)
-            sn2 = ((c4-0.39) - (1.28)*(c5 -0.393))/(0.276)
+            sn2 = ((c4/1000-0.39) - (1.28)*(c5/1000 -0.393))/(0.276)
             logger.info("{} sensor outputs {} ppb".format(self.sensor_names[2], sn2))
             # Save output to the dict
             self.sensor_output[self.sensor_names[2]] = sn2
 
             logger.info("Reading {} sensor...".format(self.sensor_names[3]))
             c6, c7 = self.read_sensor(3)
-            sn3 = ((c6 - 0.215) - (-1) * (c7 - 0.246)) / (0.266)
+            sn3 = ((c6/1000 - 0.215) - (-1) * (c7/1000 - 0.246)) / (0.266)
             logger.info("{} sensor outputs {} ppb".format(self.sensor_names[3], sn3))
             # Save output to the dict
             self.sensor_output[self.sensor_names[3]] = sn3
 
             logger.info("Reading {} sensor...".format(self.sensor_names[4]))
             c8, c9 = self.read_sensor(4)
-            sn4 = ((c8 - 0.28) - (1.82) * (c9 - 0.306)) / (0.296)
+            sn4 = ((c8/1000 - 0.28) - (1.82) * (c9/1000 - 0.306)) / (0.296)
             logger.info("{} sensor outputs {} ppb".format(self.sensor_names[4], sn4))
             # Save output to the dict
             self.sensor_output[self.sensor_names[4]] = sn4
